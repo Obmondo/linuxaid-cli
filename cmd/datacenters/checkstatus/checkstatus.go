e41fd49a091b2fa5fec2ad0952691ab0939cb971
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2024-01-11 15:48:30 +0530

    Added TLS common name support

--- cmd/datacenters/checkstatus/checkstatus.go
+++ cmd/datacenters/checkstatus/checkstatus.go
@@ -2,6 +2,7 @@ package checkstatus
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net"
 	"os"
@@ -10,48 +11,77 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// Config represents the structure of the YAML file
-type Config struct {
-	Domains map[string]string `yaml:"domains"`
+type DomainConfig struct {
+	IP         string `yaml:"ip"`
+	CommonName string `yaml:"common_name"`
 }
 
+type Config struct {
+	Domains []DomainConfig `yaml:"domains"`
+}
+
+// CheckStatus checks the status of the domains in the config file
 func CheckStatus() {
-	// Read YAML file
-	fileContent, err := os.ReadFile("domains.yaml")
+	config, err := loadConfig("config.yaml")
 	if err != nil {
-		fmt.Println("Error reading YAML file:", err)
+		fmt.Println("Error loading config:", err)
 		return
 	}
 
-	// Parse YAML content
-	var config Config
-	err = yaml.Unmarshal(fileContent, &config)
-	if err != nil {
-		fmt.Println("Error parsing YAML:", err)
-		return
-	}
-
-	// Verify TLS connection for each domain
-	for domain, ip := range config.Domains {
-		fmt.Printf("Verifying TLS connection for %s (IP: %s)\n", domain, ip)
-		if verifyTLSConnection(domain, ip) {
-			fmt.Println("TLS connection successful")
+	for _, domain := range config.Domains {
+		err := connectAndVerify(&domain)
+		if err != nil {
+			fmt.Printf("Error connecting and verifying for IP %s and common name %s: %v\n", domain.IP, domain.CommonName, err)
 		} else {
-			fmt.Println("TLS connection failed")
+			fmt.Printf("Connection successful for IP %s and common name %s. Common name matches.\n", domain.IP, domain.CommonName)
 		}
 	}
 }
 
-func verifyTLSConnection(domain, ip string) bool {
-	dialer := &net.Dialer{Timeout: 5 * time.Second}
-	conn, err := tls.DialWithDialer(dialer, "tcp", fmt.Sprintf("%s:443", ip), &tls.Config{
-		InsecureSkipVerify: true, // traefik certificates are self-signed
-	})
+// loadConfig loads the config from the given filename
+func loadConfig(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
 	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return false
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+// connectAndVerify connects to the IP address and verifies the certificate
+func connectAndVerify(domain *DomainConfig) error {
+	dialer := &net.Dialer{
+		Timeout: 5 * time.Second,
+	}
+
+	// Connect to the IP address and verify the certificate
+	conn, err := tls.DialWithDialer(dialer, "tcp", domain.IP+":443", &tls.Config{
+		InsecureSkipVerify: true,
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			cert, err := x509.ParseCertificate(rawCerts[0])
+			if err != nil {
+				return fmt.Errorf("failed to parse certificate: %v", err)
+			}
+
+			// Verify the Common Name (CN) of the certificate
+			if cert.Subject.CommonName != domain.CommonName {
+				return fmt.Errorf("common name mismatch: expected %s, got %s", domain.CommonName, cert.Subject.CommonName)
+			}
+
+			return nil
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to connect: %v", err)
 	}
 	defer conn.Close()
 
-	return true
+	return nil
 }
